53370cf34d3ecdc121301c84b66f6375c89264f9
ArtemShlyapin <artem.shlyapin@gmail.com>
2021-05-10 12:46:57 +0200

    Orange commit

--- Hello_Git.cpp
+++ Hello_Git.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, Git!" << '\n';
     std::cout << "Next commit Hello!" << '\n';
     std::cout << "Develop commit and master merge" << '\n';
+    std::cout << "Let's try to create conflict. I want orange juice" << '\n';
     return 0;
 }
